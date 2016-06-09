4fa420639216e482a90f6b256acb2683947f1b8c
Shaun Lee <shaunlee0@googlemail.com>
2016-06-09 18:14:19 +0100

    Rename package enums

--- src/main/java/com/shaun/knowledgetree/enums/CypherQueries.java
+++ src/main/java/com/shaun/knowledgetree/enums/CypherQueries.java
@@ -1,24 +1,18 @@
-package com.shaun.knowledgetree.neo4j;
+package com.shaun.knowledgetree.enums;
 
-/**
- * Created by Shaun on 04/06/2016.
- */
 public enum CypherQueries {
 
 
     DELETE_ALL_NODES_AND_RELATIONSHIPS("MATCH (n) DETACH DELETE n"),
     GET_ALL_NODES("MATCH (n) RETURN n");
-
     private String query;
 
     CypherQueries(String query) {
         this.query = query;
     }
-
     public String getQuery(){
         return query;
     }
-
     public String getQuery(String toReplace){
         return query.replace("%s",toReplace);
     }
